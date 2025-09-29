323edd3a8275db4614782c8fd62c057c2b172233
40794417KaungKhantPaing <40794417@live.napier.ac.uk>
2025-09-29 12:05:30 +0630

    Unit Test and Integration Test Workflow

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -4,28 +4,31 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppIntegrationTest
 {
     static App app;
 
     @BeforeAll
-    static void init()
+    void init()
     {
         app = new App();
-        app.connect("localhost:33060", 30000);
 
+        // Full JDBC URL
+        String jdbcUrl = "jdbc:mysql://localhost:33060/employees?useSSL=false&allowPublicKeyRetrieval=true";
+
+        // Connect to the database with 30s timeout
+        app.connect(jdbcUrl, 30000);
     }
 
     @Test
     void testGetEmployee()
     {
         Employee emp = app.getEmployee(255530);
-        assertEquals(emp.emp_no, 255530);
-        assertEquals(emp.first_name, "Ronghao");
-        assertEquals(emp.last_name, "Garigliano");
+        assertEquals(255530, emp.emp_no);
+        assertEquals("Ronghao", emp.first_name);
+        assertEquals("Garigliano", emp.last_name);
     }
 }
